3f8dec059fa589dbf5724b9a2fc4c1786dfa33d6
Leandro Giusti Mugnaini <leandromugnaini@gmail.com>
2018-12-27 15:15:21 -0200

    Revert "Revert "Projetos quarto laboratório de POO""
    This reverts commit 8fd40c22709e0eb250f135d3cb97ecdb0c4285bf.

---
+++ Mundo Yoshi/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Leandro Giusti Mugnaini
+ * Programação Orientada a Objetos
+ * Laboratório 13/09
+ */
+class Main
+{
+    public static void main(String[] args) {
+        MundoYoshi yoshi_mundo = new MundoYoshi();
+        yoshi_mundo.cenario();
+    }
+}
